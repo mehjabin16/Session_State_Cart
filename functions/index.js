1f5381772171a3c22940cc12742432b6aaf3427d
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2021-01-10 14:52:25 +0600

    all cart functionalities done

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ app.use(cors({origin:true}))
 var guid = require("guid");
 const bodyparser = require("body-parser");
 app.use(bodyparser.json());
-var itemcount = 0;
+
 
 app.post('/cart/add/:product', (req,res) =>{
     (async()=>{
@@ -32,12 +32,9 @@ app.post('/cart/add/:product', (req,res) =>{
                   const cartRef = db.collection('cart');   
                   await cartRef.add(
                     {   
-                        products: [{
                                productName: item,
-                               productID:req.body.productID,
                                productCount: 1,
-                    }],
-                        sessionID: session
+                               sessionID: session
                     } )
                     .then(() => {
                         res.writeHead(200, {
@@ -50,33 +47,45 @@ app.post('/cart/add/:product', (req,res) =>{
             var session = req.headers['session-id']
             console.log(session);
             var id ='';
-            var productList =[]
+            var itemCount = 0;
             const cartRef = db.collection('cart');
             const snapshot = await cartRef
-            .where('sessionID', '==', session)
-            .get()
-            .then(function(querySnapshot) {
+               .where('sessionID', '==', session).where('productName', '==', item)
+               .get()
+               .then(function(querySnapshot) {
                 querySnapshot.forEach(function(doc) {
-                    id = doc.id;
-                    productList = doc.data().products;
-                    console.log(doc.data().products);
-
+                    id = doc.id
+                    itemCount = doc.data().productCount
+                    console.log(id);
+                    console.log(itemCount);
                 });
-            })
-            await cartRef.doc(id).set(
-                {   
-                    products: [...productList,{
-                           productName: item,
-                           productID:req.body.productID,
-                           productCount: 1,
-                           }],
-                   
-                },{ merge: true } )
+            }) 
+               
+            if(id=='')   {
+                await cartRef.add(
+                    {   
+                               productName: item,
+                               productCount: 1,
+                               sessionID: session
+                    } )
+                    .then(() => {
+                        res.status(200).send('item added to the cart');
+                       
+                    })         
+            } 
+            else{
+                await cartRef.doc(id)
+                    .update(
+                    {
+                        productCount: itemCount+1,  
+                    }
+                )
                 .then(() => {
-                    res.status(200).send('success')
-                })        
+                    res.status(200).send('item increased');
+                   
+                })      
             }
-                                         
+            }                               
         }
             catch(error){
                 console.log(error);
@@ -90,24 +99,81 @@ app.get('/cart', (req,res) =>{
         (async()=>{
             try{
                 var session = req.headers['session-id']
-                var response=[];
+                var productList=[];
+                if ((req.headers['session-id']) == undefined || (req.headers['session-id']) == "") {
+                    res.send("No session exists. Provide valid session ID")
+                }
+                else {
+                
                 const cartRef = db.collection('cart');
                 const snapshot = await cartRef
                 .where('sessionID', '==', session)
                 .get()
                 .then(function(querySnapshot) {
                 querySnapshot.forEach(function(doc) {
-                    response = doc.data().products;
-                    //console.log(doc.data().products);
+                    let temp =[];
+                    temp ={
+                        'productName' : doc.data().productName,
+                        'productCount': doc.data().productCount
+                   }
+                    productList.push(temp);
+                    //console.log(doc.data());
                 });
             })
-                return res.status(201).send(response)
+                return res.status(200).send(productList)
     
             }
+        }
             catch(error){
                 return res.status(500).send(error)
             }
         
+    })();
+});
+app.post('/cart/decrease/:item', (req, res) => {
+    (async()=>{
+        try{
+            var item = req.params.item
+            var session = req.headers['session-id']
+            var id ='';
+            if ((req.headers['session-id']) == undefined || (req.headers['session-id']) == "") {
+                res.send("No session exists. Provide valid session ID")
+            }
+            else {
+                const cartRef = db.collection('cart');
+                const snapshot = await cartRef
+               .where('sessionID', '==', session).where('productName', '==', item)
+               .get()
+               .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    id = doc.id
+                    itemCount = doc.data().productCount
+                    //console.log(id);
+                    //console.log(itemCount);
+                });
+            }) 
+            if(id=='')   {
+                return res.status(200).send("Product doesn't exist in cart")
+            }
+            else{
+            await cartRef.doc(id)
+                    .update(
+                    {
+                        productCount: itemCount-1,  
+                    }
+                )
+                .then(() => {
+                    res.status(200).send('item decreased');
+                })       
+            }
+        }
+        
+        }
+        catch(error){
+            return res.status(500).send(error)
+        }
+
+      
     })();
 });
 
@@ -116,13 +182,33 @@ app.delete('/cart/remove/:item', (req, res) => {
         try{
             var item = req.params.item
             var session = req.headers['session-id']
-        
+             var id ='';
             if ((req.headers['session-id']) == undefined || (req.headers['session-id']) == "") {
-                res.send("No Session. Provide Valid Session ID")
+                res.send("No session exists. Provide valid session ID")
             }
             else {
-                //
+                const cartRef = db.collection('cart');
+                const snapshot = await cartRef
+               .where('sessionID', '==', session).where('productName', '==', item)
+               .get()
+               .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    id = doc.id
+                    console.log(id);
+                });
+            })  
+            if(id=='')   {
+                return res.status(200).send("Product doesn't exist in cart")
             }
+              else{
+                await cartRef.doc(id).delete().then(function() {
+                    console.log("Product removed");
+                    return res.status(200).send("Product removed!")
+                }).catch(function(error) {
+                    console.error("Error removing document: ", error);
+                });
+            }
+         }
         
         }
         catch(error){
